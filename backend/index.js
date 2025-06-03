823fba4edfa924ed80a1bd2a2a36f51ce1393d3c
Trilochan Sai <trilochansai2003@gmail.com>
2025-06-03 12:55:18 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: "https://jobportal-71tk.onrender.com",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
